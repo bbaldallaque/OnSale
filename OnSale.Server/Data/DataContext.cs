d7330a44cf9ff5034a0aec93272c582297df97dd
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2020-08-11 23:42:16 -0400

    DataContext

---
+++ OnSale.Server/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Model.Entities;
+
+namespace OnSale.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+               .HasIndex(t => t.Name)
+               .IsUnique();
+        }
+    }
+}
+
