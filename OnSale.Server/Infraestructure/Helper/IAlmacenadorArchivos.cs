2cee3f3592afe1f66532521838c1900415a69fe9
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2020-08-31 17:40:54 -0400

    update

--- OnSale.Server/Infraestructure/Helper/IAlmacenadorArchivos.cs
+++ OnSale.Server/Infraestructure/Helper/IAlmacenadorArchivos.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace OnSale.Server.Infraestructure.Helper
 {
-    interface IAlmacenadorArchivos
+    public interface IAlmacenadorArchivos
     {
+        Task<string> EditarArchivo(byte[] contenido, string extension, string contenedor, string ruta,
+            string contentType);
+        Task BorrarArchivo(string ruta, string contenedor);
+        Task<string> GuardarArchivo(byte[] contenido, string extension, string contenedor, string contentType);
     }
 }
