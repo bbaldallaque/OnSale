d65a4291c7a4e80e2d185d292b83b6e64eff2b41
Bryant Baldallaque <38927423+bbaldallaque@users.noreply.github.com>
2020-09-02 00:52:46 -0400

    update

---
+++ OnSale.Common/Services/IApiService.cs
@@ -0,0 +1,10 @@
+﻿using OnSale.Common.Responses;
+using System.Threading.Tasks;
+
+namespace OnSale.Common
+{
+    public interface IApiService
+    {
+        Task<Response> GetListAsync<T>(string urlBase, string servicePrefix, string controller);
+    }
+}
---
+++ OnSale.Prism/OnSale.Prism/App.xaml.cs
@@ -0,0 +1,33 @@
+using Prism;
+using Prism.Ioc;
+using OnSale.Prism.ViewModels;
+using OnSale.Prism.Views;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Forms;
+
+namespace OnSale.Prism
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+            containerRegistry.Register<IApiService, ApiService>();
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
